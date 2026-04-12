146096405a2c6c979c780cded1a03ba170205447
Alan Ambrose <alanambrosedev@gmail.com>
2026-04-12 20:34:02 +0530

    Add Category and Brand models, migration files, and category listing endpoint

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::orderBy('created_at', 'desc')->get();
+        return response()->json([
+            'status' => 200,
+            'data' => $categories
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\admin\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/admin/login', [AuthController::class, 'authenticate']);
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::group(['middleware:auth:santum'], function () {
+    Route::get('/categories', [CategoryController::class, 'index']);
+});
