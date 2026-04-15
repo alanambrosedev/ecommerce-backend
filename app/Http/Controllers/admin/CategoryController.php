0f8d4b8be4b8e8612a706c2ce7c6c54df993412d
Alan Ambrose <alanambrosedev@gmail.com>
2026-04-15 19:33:17 +0530

    Add Auth facade import in CategoryController

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
