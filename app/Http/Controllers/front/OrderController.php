6ad3ac4c4ab96953c80fb798e6ecb9bb01b9cd35
Alan Ambrose <alanambrosedev@gmail.com>
2026-06-30 23:00:56 +0530

    feat(api): implement OrderController with saveOrder method

---
+++ app/Http/Controllers/front/OrderController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function saveOrder(Request $request) {}
+}
