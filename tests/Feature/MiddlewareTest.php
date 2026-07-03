0b024fdf7c9ccbee23ee088b5891241a8f336f29
Alan Ambrose <alanambrosedev@gmail.com>
2026-07-03 22:36:02 +0530

    fix(tests): reorder use statements in MiddlewareTest for consistency

--- tests/Feature/MiddlewareTest.php
+++ tests/Feature/MiddlewareTest.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Models\User;
-use Laravel\Sanctum\Sanctum;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Laravel\Sanctum\Sanctum;
 
 uses(DatabaseTransactions::class);
 
