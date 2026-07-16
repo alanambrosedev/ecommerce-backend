212674ad0bfa183d1a9cd733c3456b2dbab8c9d2
Alan Ambrose <alanambrosedev@gmail.com>
2026-07-16 19:27:59 +0530

    feat(api): add getOrderDetails method to retrieve order information

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,4 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model {}
+class Order extends Model
+{
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function casts()
+    {
+        return [
+            'created_at' => 'datetime:d M, Y',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -33,4 +33,5 @@ Route::group(['middleware' => ['auth:sanctum', 'checkAdminRole']], function () {
 });
 Route::group(['middleware' => ['auth:sanctum', 'checkUserRole']], function () {
     Route::post('/save-order', [OrderController::class, 'saveOrder']);
+    Route::post('/order-details/{id}', [OrderController::class, 'getOrderDetails']);
 });
