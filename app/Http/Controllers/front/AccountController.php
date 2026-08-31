a5b5a451760dd68e38b4f63d7a97d8d3a6875a5d
Alan Ambrose <alanambrosedev@gmail.com>
2026-08-31 20:20:05 +0530

    feat: implement customer account management and authentication system with User model and validation requests

--- app/Http/Controllers/front/AccountController.php
+++ app/Http/Controllers/front/AccountController.php
@@ -3,41 +3,28 @@
 namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AccountRequest;
 use App\Models\Order;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class AccountController extends Controller
 {
-    public function register(Request $request)
+    public function register(AccountRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 400,
-                'errors' => $validator->errors(),
-            ]);
-        }
-
-        $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->role = 'customer';
-        $user->save();
+        $user = User::create([...$request->validated(), 'role' => 'customer']);
 
         return response()->json([
-            'status' => 201,
             'message' => 'User created successfully.',
-        ], 201);
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ],
+        ], Response::HTTP_CREATED);
     }
 
     public function authenticate(Request $request)
